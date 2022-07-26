fe88d15e828df1ce7f34ebe3b2423ec350ae2bdf
dinhdjj <dinhdjj@gmail.com>
2022-07-26 14:16:05 +0700

    product: index feature

---
+++ QLBHLeVanDinh/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using QLBHLeVanDinh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBHLeVanDinh.Controllers
+{
+    public class ProductController : Controller
+    {
+        NWDataClassesDataContext da = new NWDataClassesDataContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var ps = da.Products.ToList();
+            return View(ps);
+        }
+    }
+}
